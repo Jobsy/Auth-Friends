be438d7df0f23dac3f82d1da4c69b61489c3855b
Oluwajoba <oluwajobabello@gmail.com>
2019-10-15 18:00:37 +0100

    add FriendsList NavLink and Route / FiendsList / Container.js

--- friends/src/components/Container.js
+++ friends/src/components/Container.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, NavLink, withRouter, Redirect } from "react-router-dom";
 import LoginForm from "./LoginForm";
-
+import  FriendsList from "./FriendsList";
 
 export function Container(props) {
   // const 
@@ -10,12 +10,24 @@ export function Container(props) {
     <div>
       <span>
         <NavLink exact to="/">Login</NavLink>
-      </span>
+        <NavLink exact to="/friends">Friends List</NavLink>
 
+      </span>
       <main>
-        <Route exact path="/" component={LoginForm}/>
+        <Route exact path="/friends" component={LoginForm} />
+        <Route exact path="/"
+        render={props => withAthCheck(FriendsList, props)} />
       </main>
     </div>
-  )
+  );
 }
 
+function withAthCheck(Component, props) {
+  if(localStorage.getItem("token")) {
+    return <Component {...props} />
+  }
+  return <Redirect to="/" />;
+}
+
+
+export default withRouter(Container);
