c4c41067483d9dc4defb368838fbd25b48a3dcc2
Oluwajoba <oluwajobabello@gmail.com>
2019-10-15 14:40:54 +0100

    build out a simple login form with username and password inputs and a submit button / LoginForm.js

---
+++ friends/src/components/LoginForm.js
@@ -0,0 +1,38 @@
+
+import React, {useRef} from "react";
+import axios from "axios";
+
+export default function LoginForm(props) {
+    const userNameRef = useRef();
+    const passwordRef = useRef();
+
+    const submit = () => {
+        axios.post("http://localhost:5000/api/login", {
+            userName: userNameRef.current.value,
+            password: passwordRef.current.value,
+        })
+        .then(res => {
+            // debugger
+            localStorage.setItem("token", res.data.token)
+            props.history.push("/");
+        });
+    };
+
+    return (
+        <div>
+            <div>
+                Username <input ref={userNameRef} type="
+                text" />
+                <br />
+
+                Password  <input ref={passwordRef} type="
+                text" />
+                <br />
+            </div>
+
+            <div>
+                <button onClick={submit}>Submit</button>
+            </div>
+        </div>
+    );
+}
