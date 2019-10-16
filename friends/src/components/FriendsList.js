c47d77231291f813e7cbe9acf008d5afe0c4b363
Oluwajoba <oluwajobabello@gmail.com>
2019-10-16 03:07:35 +0100

    create friends form component / FriendsForm / friendsList.js

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,33 +1,114 @@
 
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axiosWithAuth from "../axios";
+import axios from "axios";
 
 
 
 export default function FriendsList(props) {
-    const [friends, setFriends] = useState([]);
+  const [friends, setFriends] = useState([]);
 
 
-    useEffect(() => {
+  useEffect(() => {
 
-        axiosWithAuth().get("http://localhost:5000/api/friends")
-        .then(res => {
-            // debugger
-            console.log(">>>: ", res)
-            setFriends(res.data);
-        })
-        .catch(error => {
-            alert("Sorryyyyyyyyyy")
-        });
-    }, []);
+    axiosWithAuth().get("http://localhost:5000/api/friends")
+      .then(res => {
+        // debugger
+        console.log(">>>: ", res)
+        setFriends(res.data);
+      })
+      .catch(error => {
+        alert("Sorryyyyyyyyyy")
+      });
+  }, []);
 
-    return (
+  return (
+    <div>
+      {
+        friends.map(friend => (
+          <div>
+            {/* {console.log(friend)} */}
+            Name: {friend.name}
+            <br />
+            Age: {friend.age}
+            <br />
+            Email: {friend.email}
+            <br />
+            <br />
+          </div>
+        ))
+      }
+      <FriendsForm />
+    </div>
+  )
+}
+
+
+
+function FriendsForm(props) {
+  const [newFriend, setNewFriend] = useState(
+    {
+      name: "",
+      age: "",
+      email: ""
+    }
+  )
+
+  const changeHandler = e => {
+    setNewFriend({
+      ...newFriend,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const submitHandler = e => {
+    e.preventDefault()
+    axios
+    .post('http://localhost:5000/api/friends', newFriend)
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  return (
+    <>
+      <h3>Friends Membership Form</h3>
+      <form onSubmit={submitHandler}>
         <div>
-            {
-                friends.map(friend => (
-                    console.log(friend)
-                ))
-            }
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            value={newFriend.name}
+            onChange={changeHandler}
+          />
         </div>
-    )
+
+        <div>
+          <label>Age</label>
+          <input
+            type="text"
+            name="age"
+            value={newFriend.age}
+            onChange={changeHandler}
+          />
+        </div>
+
+        <div>
+          <label>Email</label>
+          <input
+            type="text"
+            name="email"
+            value={newFriend.email}
+            onChange={changeHandler}
+          />
+        </div>
+        <button type="submit">Submit</button>
+
+      </form>
+    </>
+  )
 }
